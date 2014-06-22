69077e1e3e02c65b773491f54b8b4207731867e6
tabasano <pianotoieba@gmail.com>
2014-06-23 00:04:38 +0900

    midi tempo up-down

--- midi-simple-make.rb
+++ midi-simple-make.rb
@@ -12,6 +12,8 @@ def sizemake d
 #  p [d,i,i.to_s(16)]
   ("00000000"+i.to_s(16))[-8..-1]
 end
+# 可変長数値表現
+# 7bitずつに区切り最後以外のbyteは先頭bitを立てる
 def varlen(v)
   if v < 0x80
     return v
@@ -40,22 +42,23 @@ def txt2hex t
 end
 
 module Mid
-  def self.header format,track,size
+  def self.header format,track,tbase=480
     format=[format,0xff].min
     track=[track,0xff].min
-    size=[size,0xffff].min
+    tbase=[tbase,0x7fff].min
+    @tbase=tbase
     format=format("%02x",format)
     track=format("%02x",track)
-    size=format("%04x",size)
+    tbase=format("%04x",tbase)
     "
       4D 54 68 64  # ヘッダ
       00 00 00 06  # データ長:6[byte]
       00 #{format} # フォーマット
       00 #{track}  # トラック数
-      #{size}      # 1 拍の分解能
+      #{tbase}      # 1 拍の分解能
     "
   end
-  def self.oneNote len=480,key=0x3C,velocity=40,ch=0
+  def self.oneNote len=@tbase,key=@basekey,velocity=0x40,ch=0
     ch=[ch,0x0f].min
     velocity=[velocity,0x7f].min
     key=[key,0x7f].min
@@ -65,11 +68,11 @@ module Mid
     delta=varlenHex(len)
     str="
       00 9#{ch} #{key} #{velocity} # 0拍後, soundオン...
-      #{delta} 80 #{key} 00 # delta後, soundオフ
+      #{delta} 8#{ch} #{key} 00 # delta後, soundオフ
     "
   end
   def self.notekey key
-    @set||=[480,40,0]
+    @set||=[@tbase,40,0]
     len,velocity,ch=@set
     if key.class==Fixnum
       self.oneNote(len,@basekey+key,velocity,ch)
@@ -99,40 +102,60 @@ module Mid
     }
     notekey(@notes[c])
   end
-  def self.rest len=480
+  def self.rest len=@tbase
     delta=varlenHex(len)
     "
       #{delta}  89 3C 00 # 1拍後, オフ:ch10, key:3C
     "
   end
+  def self.tempo bpm
+    @bpm=bpm
+    d_bpm=self.makebpm(@bpm)
+    "
+      00 FF 51 03 #{d_bpm} # 四分音符の長さをマイクロ秒で3byte
+    "
+  end
+  def self.makebpm bpm
+    d="000000"+(60_000_000/bpm.to_f).to_i.to_s(16)
+    d[-6..-1]
+  end
   def self.makefraze rundata
-    r=[]
+    @h=[]
     @basekey||=0x3C
-    rundata.scan(/[0-9]+|[-+a-zA-Z]/).each{|i|
+    rundata.scan(/[0-9]+|[-+a-zA-Z><]/).each{|i|
       case i
+      when /</
+        @bpm=@bpm/1.25
+        @h<<self.tempo(@bpm)
+      when />/
+        @bpm=@bpm*1.25
+        @h<<self.tempo(@bpm)
       when /-/
         @basekey-=12
       when /\+/
         @basekey+=12
       when /[0-9]+/
-        (i.to_i-1).times{r<<r[-1]}
+        (i.to_i-1).times{@h<<@h[-1]}
       when "r"
-        r<<self.rest
+        @h<<self.rest
       when " "
       else
-        r<<self.notes(i)
+        @h<<self.notes(i)
       end
     }
-    r*"\n# onoff ==== \n"
+    @h*"\n# onoff ==== \n"
+  end
+  def self.dumpHex
   end
 end
 
 array = []
 
-d_head=Mid.header(1,1,480)
+tbase=480
+d_head=Mid.header(1,1,tbase)
 
-delta=varlenHex(480)
-p "deltaTime: 0x#{delta}"
+delta=varlenHex(tbase)
+#p "deltaTime: 0x#{delta}"
 d_start="
 4D 54 72 6B # トラック 1 開始
 "
@@ -142,9 +165,6 @@ comment="by midi-simple-make.rb"
 commenthex,len=txt2hex(comment)
 d_comment="
 00 FF 01 #{len} #{commenthex}
-"
-d_tempo="
-00 FF 51 03  07 A1 20 #bpm=120, 四分音符の長さをマイクロ秒で3byte
 "
 d_last=
 "
@@ -158,19 +178,11 @@ def hint
   puts "usage: #{$0} 'dddd dr3 dddd r4 drdrdrdr dddd dr3' outfile.mid bpm"
   puts "    abcdefg=sound, +-=octave change, r=rest, num=length, blank ignored"
 end
-def makebpm bpm
-  d="000000"+(60_000_000/bpm.to_f).to_i.to_s(16)
-  d[-6..-1]
-end
 rundata,ofile,bpm = ARGV
 (hint;exit) if ! rundata
 
 bpm=120 if ! bpm
-d_bpm=makebpm(bpm)
-
-d_tempo="
-00 FF 51 03 #{d_bpm} # 四分音符の長さをマイクロ秒で3byte
-"
+d_tempo=Mid.tempo(bpm)
 
 d_data = d_comment + d_tempo + Mid.makefraze(rundata) + d_last
 d_dsize=sizemake(d_data)
