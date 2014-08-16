8cb525b1e51d6d01bdd7bde9fa53bdc80eceef0f
tabasano <pianotoieba@gmail.com>
2014-08-16 17:00:07 +0900

    midi: msm more simple.

---
+++ midi-simple-make-simple.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+# -*- encoding: utf-8 -*-
+require 'kconv'
+require 'optparse'
+require './lib/msm'
+
+infile="infile.mml"
+outfile="mml.mid"
+data=""
+
+opt = OptionParser.new
+opt.on('-i file',"input file") {|v| infile=v }
+opt.on('-o file',"output file") {|v| outfile=v }
+opt.on('-d d',"input data string") {|v| data=v }
+opt.parse!(ARGV)
+
+mtr=MmlTracks.new
+mtr.infile=infile
+mtr.outfile=outfile
+mtr.data=data
+mtr.make
+mtr.save
