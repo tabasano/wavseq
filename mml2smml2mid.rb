5d758a5cbeef98976cb43396fa60828319cb1bcf
tabasano <pianotoieba@gmail.com>
2014-08-20 22:39:43 +0900

    midi: mml2smml repeat.

--- mml2smml2mid.rb
+++ mml2smml2mid.rb
@@ -23,6 +23,7 @@ def help
 
     #  comment
     +- sharp,flat
+    []3 repeat 3 times
   )
 end
 
@@ -44,33 +45,38 @@ def p *v
   super v if $DEBUG
 end
 
-def calclen len,digdef
-  htn=0
-  if len=~/\.+/
-    len=$`.to_i
-    len=digdef if len==0
-    htn=$&.size
+def calclen len,digdef,last
+  if last == :sound
+    htn=0
+    if len=~/\.+/
+      len=$`.to_i
+      len=digdef if len==0
+      htn=$&.size
+    end
+    len=4.0/len.to_i
+    baselen=len/2.0
+    htn.times{len+=baselen;baselen/=2.0}
+    len=len.round if len==len.round
+    len="" if len==1
   end
-  len=4.0/len.to_i
-  baselen=len/2.0
-  htn.times{len+=baselen;baselen/=2.0}
-  len=len.round if len==len.round
-  len="" if len==1
   len
 end
 def mml2smml data
   tracks={}
   tr=0
+  last=:no
   lines=data.split("\n")
   lines.each{|mml|
     octave=3
     digdef=4
-    mml.scan(/([[:alpha:]]):|([[:alpha:]<>])([#\+\-])?([[:digit:]\.]+)?|@([[:alpha:]]+)|([[:digit:]\.]+)| |/).each{|tname,al,sharp,dig,sname,dig2|
-      value=[tname,al,sharp,dig,sname,dig2]*""
+    mml.scan(/([[:alpha:]]):|([[:alpha:]<>])([#\+\-])?([[:digit:]\.]+)?|@([[:alpha:]]+|[[:digit:]]+)|([[:digit:]\.]+)|([\[\]])| |/).each{|tname,al,sharp,dig,sname,dig2,same|
+      valueAr=[tname,al,sharp,dig,sname,dig2,same]
+      value=valueAr*""
       (tr=tname;next) if tname
       len=dig|| digdef
-      len=calclen(len,digdef)
-      dig2=calclen(dig2,digdef) if dig2
+      len=calclen(len,digdef,:sound)
+      dig2=calclen(dig2,digdef,last) if dig2
+      last=:no
       v=""
       case al ? al.upcase : al
       when ">"
@@ -105,10 +111,14 @@ def mml2smml data
       when "R"
         v="r#{len}"
         p "rest  #{len} : #{v}"
+        last=:sound
       when nil
         if sname
           v="(p:#{sname})"
           p "sound name #{sname} : #{v}"
+        elsif same
+          v=same
+          p "same : #{v}"
         end
       when "A".."G"
         note=al.downcase
@@ -120,6 +130,7 @@ def mml2smml data
         end
         v="#{note}#{len}"
         p "note #{al} #{sharp}  #{len} : #{v}"
+        last=:sound
       end
       v="#{dig2}" if dig2
       STDERR.puts "[#{value}] undefined?" if v=="" && value.size>0
@@ -130,7 +141,7 @@ def mml2smml data
       end
     }
   }
-  tracks.values*"\n|||\n"
+  tracks.keys.sort.map{|k|tracks[k]}*"\n|||\n"
 end
 
 m=MmlTracks.new
