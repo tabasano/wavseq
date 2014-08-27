14a3a254002597a750f641349544d8e3877b24f5
tabasano <pianotoieba@gmail.com>
2014-08-27 16:38:50 +0900

    midi: MmlEventM ci.

--- midi-after-test.sh
+++ midi-after-test.sh
@@ -3,3 +3,4 @@ gem install pkg/smml*gem -l
 ruby -rubygems -e 'require"smml"; p Smml::VERSION; m=Smml.new;m.compile("sample/midi-test.mml","gem.mid")'
 cat mml2smml.mml
 hexdump gem.mid -C | head -n 7
+ruby workdir/regexpMmlEventModuleTest.rb
